b51da06308f85c4f736a8d4bb2158aa0ff0386c3
bdrodriguez-cthr <brodriguez.cthr@gmail.com>
2019-11-06 15:37:18 -0500

    preview.js: div.page are being converted to canvas properly. Next: print as pdf

--- HTML_HID_Automator/public/javascripts/preview.js
+++ HTML_HID_Automator/public/javascripts/preview.js
@@ -1,14 +1,21 @@
-// alert("I'm connected");
+// import html2canvas from "html2canvas";
 
+alert("I'm connected");
+// var html2canvas = require('html2canvas');
 //onload
 window.onload = function() {
-
     var hidsArr = document.getElementsByClassName("page");
-
+    
+    var body = document.getElementsByTagName("body")[0];
     for(var i = 0; i < hidsArr.length; i++) {
-        html2canvas(hidsArr[i].then(function(canvas) {
-            document.body.appendChild(canvas);
-        }));
+        html2canvas(hidsArr.item(i))
+            .then(function(canvas) {
+                body.appendChild(canvas);
+            });
+    }
+    
+    for(var i = 0; i < 49; i++) {
+        document.getElementsByClassName("page")[0].remove()
     }
 }
 
